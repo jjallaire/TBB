96b46714802aad40400f5f5236c008d52e6ad9fb
JJ Allaire <jj@rstudio.org>
2014-04-19 14:18:09 -0400

    work on loading c++ symbols via R_RegisterCCallable

--- inst/examples/parallel_for.cpp
+++ inst/examples/parallel_for.cpp
@@ -1,22 +1,8 @@
-#include <Rcpp.h>
-using namespace Rcpp;
-
-// [[Rcpp::depends(tbb)]]
-
 #include <tbb/tbb_stddef.h>
 
-// Below is a simple example of exporting a C++ function to R. You can
-// source this function into an R session using the Rcpp::sourceCpp 
-// function (or via the Source button on the editor toolbar)
-
-// For more on using Rcpp click the Help button on the editor toolbar
+extern "C" DL_FUNC _ZN3tbb8internal15runtime_warningEPKcz = R_GetCCallable("tbb", "_ZN3tbb8internal15runtime_warningEPKcz");
 
 // [[Rcpp::export]]
-int timesTwo(int x) {
-   
-   //R_GetCCallable("tbb", "__ZN3tbb8internal15runtime_warningEPKcz");
-   
+void testTbb() {
    tbb::internal::runtime_warning("no way!!!");
-   
-   return 0;
 }
